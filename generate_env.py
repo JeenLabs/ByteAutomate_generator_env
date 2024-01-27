0dd7497498948b9b12b0409d773a95c2a88a22a3
mrjarvis698 <dudamajaykumar59@gmail.com>
2024-01-27 19:51:49 +0530

    env_gen: update key values instead of append to avoid dups.
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- generate_env.py
+++ generate_env.py
@@ -6,23 +6,36 @@ class EnvFileManager:
 
     @classmethod
     def save_to_env_file(cls, key, value):
-        # Check if the .env file exists, create one if not
-        if not os.path.isfile(cls.ENV_FILE_NAME):
-            with open(cls.ENV_FILE_NAME, 'w') as file:
-                file.write("# This is a ByteAutomate .env file\n")
+        env_data = {}  # To store key-value pairs from the existing .env file
 
-        # Append the new key-value pair to the .env file
-        with open(cls.ENV_FILE_NAME, 'a') as file:
-            file.write(f"{key}={value}\n")
+        # Check if the .env file exists
+        if os.path.isfile(cls.ENV_FILE_NAME):
+            with open(cls.ENV_FILE_NAME, 'r') as file:
+                # Read existing key-value pairs from the .env file
+                for line in file:
+                    line = line.strip()
+                    if line and not line.startswith("#"):
+                        parts = line.split('=')
+                        if len(parts) == 2:
+                            env_data[parts[0]] = parts[1]
+
+        # Update the value of the key or append a new line if the key doesn't exist
+        env_data[key] = value
+
+        # Write the updated key-value pairs back to the .env file
+        with open(cls.ENV_FILE_NAME, 'w') as file:
+            file.write("# This is a ByteAutomate .env file\n")
+            for k, v in env_data.items():
+                file.write(f"{k}={v}\n")
 
     @classmethod
     def main(cls):
-        predefinekey = ["DatabaseName", "DatabasePath"]
-        for key in predefinekey:
+        predefine_keys = ["DatabaseName", "DatabasePath"]
+        for key in predefine_keys:
             value = input(f"Enter the value for '{key}' : ")
             cls.save_to_env_file(key, value)
             print(f"Environment variable '{key}' with value '{
-                value}' saved to {cls.ENV_FILE_NAME}")
+                  value}' saved to {cls.ENV_FILE_NAME}")
 
 
 # Run the program
